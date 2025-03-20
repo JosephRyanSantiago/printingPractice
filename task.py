4bed4a2683bfac3d3f1133f0fccd44a842b4fd14
Joseph Santiago <curlyjrs92@gmail.com>
2025-03-20 01:10:00 -0500

    added band project and completed day 1

--- task.py
+++ task.py
@@ -1,3 +1,21 @@
 # print("Hello world!\nHello world!\nHello world!")
 # print("Hello" + " "+ "Joe!")
-print("Hello " + input("What is your name?") + "!")
+# print("Hello " + input("What is your name? ") + "!")
+# name = input("What is your name?")
+# print(name)
+
+# name = "Joe"
+# print (name)
+
+# name = "Joey"
+# print(name)
+
+# print(len(input("What is your name? ")))
+
+# username = input("What is your name?")
+# length = len(username)
+# print(length)
+print("Welcome to the Band Name Generator.")
+animal = input("What is your favorite animal? ")
+food = input("What is your favorite food? ")
+print("Your band name could be: " + animal + " " + food)
